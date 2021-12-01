8c8fcffd194f76c01cf21580a31527f395c9dfb5
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2021-12-01 08:59:00 +0200

    updated files

--- Developed/Newsletter-Signup/app.js
+++ Developed/Newsletter-Signup/app.js
@@ -2,10 +2,60 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const reuquest= require('request');
+const request = require("request");
+const https = require("https");
 
 const app = express();
 
-app.listen(3000, function() {
-  console.log("Server is running on port 3000.");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
 });
+
+app.post("/", function(req, res) {
+
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+      members: [{
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        }
+      }
+    ]
+  };
+
+  console.log(firstName, lastName, email);
+
+  const jsonData = JSON.stringify(data);
+  const url = "https://us20.api.mailchimp.com/3.0/lists/556ed07dad";
+  const options = {
+    method: "POST",
+    auth: "aurimas13:3dcbc46278b08c1378901ffd90deb775-us20"
+  };
+  const request = https.request(url, options, function(response) {
+      response.on("data", function(data) {
+          console.log(JSON.parse(data));
+      })
+  });
+
+  request.write(jsonData); request.end();
+
+});
+
+app.listen(1000, function() {
+  console.log("Server is running on port 1000");
+});
+
+// API Key
+// 3dcbc46278b08c1378901ffd90deb775-us20
+
+// List Id
+// 556ed07dad
--- Developed/WeatherProject/app.js
+++ Developed/WeatherProject/app.js
@@ -49,6 +49,7 @@ app.post("/", function(req, res){
    })
 });
 
+
 app.listen(3000, function() {
   console.log("Server is running on port 3000.");
 });
