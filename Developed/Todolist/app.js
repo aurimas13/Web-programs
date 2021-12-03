8699e049024ad9bbf7d5a4b6a3fdd2e679f80e1b
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2021-12-03 09:24:11 +0200

    updated main branch files

---
+++ Developed/Todolist/app.js
@@ -0,0 +1,36 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+
+const app = express();
+
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+
+  var today = new Date();
+  var options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+  var day = today.toLocaleDateString("en-US", options);
+
+  res.render('list', {kindOfDay: day, newListItems: items});
+
+});
+
+app.post("/", function(req, res) {
+  var item = req.body.newItem;
+  items.push(item);
+  console.log(item);
+  res.redirect("/");
+});
+
+
+app.listen(2222, function() {
+  console.log("Server started on port 2222.");
+});
---
+++ Developed/Todolist/template.js
@@ -0,0 +1,16 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/", function(req, res){
+  res.send("Hello");
+});
+
+
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000.");
+});
